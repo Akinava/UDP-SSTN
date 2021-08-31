6c93e794797323725381878eb9ad3e02e3af7e2b
Andrei Chernov <Andrei.Chernov1@thomsonreuters.com>
2021-08-31 21:18:30 +0200

    handle stream

--- src/server_handler.py
+++ src/server_handler.py
@@ -12,33 +12,35 @@ from settings import logger
 
 
 class ServerHandler(Handler):
-    def hpn_neighbour_client(self):
+    def hpn_neighbour_client(self, connection):
         #logger.info('')
-        self.__set_pub_key_to_connection()
-        self.__set_encrypt_marker_to_connection()
-        self.neighbour_connection = self.net_pool.find_neighbour(self.connection)
-        if self.neighbour_connection:
-            logger.info('found neighbour {} for peer {}'.format(self.neighbour_connection, self.connection))
-            self.__send_hpn_neighbour_client_response()
-            self.__handle_disconnect()
+        self.__set_pub_key_to_connection(connection)
+        self.__set_encrypt_marker_to_connection(connection)
+        neighbour_connection = self.net_pool.find_neighbour(connection)
+        print('>'*10, 'hpn_neighbour_client', connection, connection.get_pub_key())
+        if neighbour_connection:
+            print('>'*10, 'hpn_neighbour_client', neighbour_connection, neighbour_connection.get_pub_key())
+            logger.info('found neighbour {} for peer {}'.format(neighbour_connection, connection))
+            self.__send_hpn_neighbour_client_response(connection, neighbour_connection)
+            self.__handle_disconnect(neighbour_connection, connection)
         else:
-            logger.info('no neighbours for peer {}'.format(self.connection))
-        self.__update_connections_state()
+            logger.info('no neighbours for peer {}'.format(connection))
+        self.__update_connections_state(connection, neighbour_connection)
 
-    def __send_hpn_neighbour_client_response(self):
-        receiver_message = self.__make_connection_message(self.connection, self.neighbour_connection)
-        neighbour_message = self.__make_connection_message(self.neighbour_connection, self.connection)
+    def __send_hpn_neighbour_client_response(self, connection1, connection2):
+        connection1_message = self.__make_connection_message(connection1, connection2)
+        connection2_message = self.__make_connection_message(connection2, connection1)
 
-        self.send(message=receiver_message,
-                  receiving_connection=self.connection,
+        self.send(message=connection1_message,
+                  receiving_connection=connection1,
                   package_protocol_name='hpn_neighbour_client')
-        self.send(message=neighbour_message,
-                  receiving_connection=self.neighbour_connection,
+        self.send(message=connection2_message,
+                  receiving_connection=connection2,
                   package_protocol_name='hpn_neighbour_client')
 
     def __make_connection_message(self, receiving_connection, neighbour_connection):
         return self.make_message(
-            package_name='hpn_neighbour_client',
+            package_protocol_name='hpn_neighbour_client',
             receiving_connection=receiving_connection,
             neighbour_connection=neighbour_connection)
 
@@ -46,29 +48,30 @@ class ServerHandler(Handler):
         return kwargs['neighbour_connection'].get_pub_key()
 
     def get_neighbour_addr(self, **kwargs):
-        return self.parser.pack_addr(kwargs['neighbour_connection'].get_remote_addr())
+        return self.parser().pack_addr(kwargs['neighbour_connection'].get_remote_addr())
 
     def get_disconnect_flag(self, **kwargs):
-        return self.parser.pack_bool(self.net_pool.can_be_disconnected(kwargs['receiving_connection']))
+        disconnect_flag = self.net_pool.can_be_disconnected(kwargs['receiving_connection'])
+        return self.parser().pack_bool(disconnect_flag)
 
-    def __set_pub_key_to_connection(self):
-        connection_pub_key = self.parser.get_part('requester_pub_key')
-        self.connection.set_pub_key(connection_pub_key)
+    def __set_pub_key_to_connection(self, connection):
+        connection_pub_key = connection.get_unpack_request_part('requester_pub_key')
+        connection.set_pub_key(connection_pub_key)
 
-    def __set_encrypt_marker_to_connection(self):
-        encrypt_marker = self.parser.get_part('encrypted_request_marker')
-        self.connection.set_encrypt_marker(encrypt_marker)
+    def __set_encrypt_marker_to_connection(self, connection):
+        encrypt_marker = connection.get_unpack_request_part('encrypted_request_marker')
+        connection.set_encrypt_marker(encrypt_marker)
 
-    def __update_connections_state(self):
-        stat = 'waiting' if self.neighbour_connection is None else 'in_progress'
-        self.__save_neighbour_connection_param(stat)
+    def __update_connections_state(self, connection, neighbour_connection):
+        stat = 'waiting' if neighbour_connection is None else 'in_progress'
+        self.__save_neighbour_connection_param(stat, connection, neighbour_connection)
 
     def __handle_disconnect(self, *connections):
         for connection in connections:
             if self.net_pool.can_be_disconnected(connection):
                 self.net_pool.disconnect(connection)
 
-    def __save_neighbour_connection_param(self, state):
-        self.net_pool.update_neighbour_group(self.connection, self.neighbour_connection)
-        self.net_pool.update_state(self.connection, state)
-        self.net_pool.update_state(self.neighbour_connection, state)
+    def __save_neighbour_connection_param(self, state, connection, neighbour_connection):
+        self.net_pool.update_neighbour_group(connection, neighbour_connection)
+        self.net_pool.update_state(connection, state)
+        self.net_pool.update_state(neighbour_connection, state)
